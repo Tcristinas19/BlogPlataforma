5f443182f0c95d41f9d68ec9cb8f2e2ab85a26c2
Thamires Santos <93771311+Tcristinas19@users.noreply.github.com>
2022-01-27 22:43:40 -0300

    componente inicio e tema

--- blogplataforma/src/app/service/auth.service.ts
+++ blogplataforma/src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { usuario } from '../model/usuario';
 import { usuarioLogin } from '../model/usuarioLogin';
 
@@ -23,5 +24,15 @@ export class AuthService {
 
   cadastrar(usuario: usuario): Observable<usuario>{
     return this.http.post<usuario>('http://localhost:8080/usuarios/cadastrar', usuario)
+  }
+
+  logado(){
+    let ok = false
+
+    if(environment.token != ''){
+      ok = true
+    }
+
+    return ok
   }
 }
